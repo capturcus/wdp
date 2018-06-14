9ea89032b24fc47d9ec7c0578120901555474896
Marcin Parafiniuk <marcin.parafiniuk@adm.uw.edu.pl>
2018-06-14 12:30:37 +0200

    <quick deploy>

--- tester/src/app/pytania.ts
+++ tester/src/app/pytania.ts
@@ -424,7 +424,7 @@ export default class Questions { public static QUESTIONS = [
         "options": [
             [
                 "a) odruchu repulsyjnego", 
-                true
+                false
             ], 
             [
                 "b) odruchu kichania", 
